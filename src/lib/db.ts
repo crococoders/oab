27e7990c0fb03ff6920643ad4f192565e24a15f2
Murat Tishkul <murat.tishkul@gmail.com>
2022-03-22 20:32:59 +0600

    added  react notion x

---
+++ src/lib/db.ts
@@ -0,0 +1,16 @@
+// import * as firestore from '@google-cloud/firestore'
+// import * as config from './config'
+//
+// export let db: firestore.Firestore = null
+// export let images: firestore.CollectionReference = null
+//
+// if (config.isPreviewImageSupportEnabled) {
+//   db = new firestore.Firestore({
+//     projectId: config.googleProjectId,
+//     credentials: config.googleApplicationCredentials
+//   })
+//
+//   images = db.collection(config.firebaseCollectionImages)
+// }
+
+export let db: { getAll: (arg0: any) => any; }
